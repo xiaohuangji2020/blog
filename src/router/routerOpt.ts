e8ccaf9b5a4c42031d91fca80b4fea79c2560ee1
roco2015 <roco2015@163.com>
2020-03-25 10:12:33 +0800

    添加各模块

--- src/router/routerOpt.ts
+++ src/router/routerOpt.ts
@@ -2,27 +2,27 @@ const router = [
     {
         path: '/t',
         name: 'technology',
-        component: () => import(/* webpackChunkName: 'inspiration' */ '../views/technology/index.vue')
+        component: () => import(/* webpackChunkName: 'technology' */ '@/views/technology/index.vue')
     },
     {
         path: '/cv',
         name: 'cv',
-        component: () => import(/* webpackChunkName: 'inspiration' */ '../views/cv/index.vue')
+        component: () => import(/* webpackChunkName: 'cv' */ '@/views/cv/index.vue')
     },
     {
         path: '/about',
         name: 'about',
-        component: () => import(/* webpackChunkName: 'inspiration' */ '../views/about/index.vue')
+        component: () => import(/* webpackChunkName: 'about' */ '@/views/about/index.vue')
     },
     {
         path: '/inspiration',
         name: 'inspiration',
-        component: () => import(/* webpackChunkName: 'inspiration' */ '../views/inspiration/index.vue')
+        component: () => import(/* webpackChunkName: 'inspiration' */ '@/views/inspiration/index.vue')
     },
     {
         path: '/other',
         name: 'other',
-        component: () => import(/* webpackChunkName: 'inspiration' */ '../views/other/index.vue')
+        component: () => import(/* webpackChunkName: 'other' */ '@/views/other/index.vue')
     }
 ];
 
