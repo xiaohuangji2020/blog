c79662cef39d684fc931200da8a4b9fa5011f59a
roco2015 <roco2015@163.com>
2020-10-14 17:32:34 +0800

    添加鼠标点击特效

--- src/views/home/Content.vue
+++ src/views/home/Content.vue
@@ -1,18 +1,15 @@
 <template>
   <main class="main">
-    <section class="sec sec1"></section>
-    <!-- <div class="sec2">
-      <ul class="intro-l">
-        <li>完全不知道要写些什么</li>
-        <li>不是文人、也不是墨客的<span class="stress">理科生</span></li>
-        <li>脑回路或者说<span class="stress">精神</span>有点不正常</li>
-      </ul>
-      <ul class="intro-r">
-        <li>就是想<span class="stress">搞</span>个博客玩玩</li>
-        <li>是<span class="stress">废柴</span>哦</li>
-        <li>自定义职介为：<span class="stress">Berserker</span></li>
-      </ul>
-    </div> -->
+    <section class="sec sec-banner"></section>
+    <section class="sec note-sec sec2">
+      文章1
+    </section>
+    <section class="sec note-sec sec3">
+      文章2
+    </section>
+    <section class="sec note-sec sec4">
+      文章3
+    </section>
   </main>
 </template>
 
@@ -29,10 +26,10 @@ export default class Content extends Vue {}
   margin: 0 auto;
   line-height: 34px;
 }
-.sec1 {
+.sec-banner {
   position: relative;
   height: 250px;
-  background: url('~@/assets/images/home/banner1.png') no-repeat center top;
+  background: url('~@/assets/images/home/banner1.png') no-repeat center;
   background-size: contain;
   border-radius: 20px;
   box-shadow: 0 22px 50px -24px rgba(0, 0, 0, 0.6);
