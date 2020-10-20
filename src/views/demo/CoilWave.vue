36c7bc62fe8cb076f3391f7231289dc89048e61f
roco2015 <roco2015@163.com>
2020-10-20 15:37:12 +0800

    波动的线圈

--- src/views/demo/CoilWave.vue
+++ src/views/demo/CoilWave.vue
@@ -9,6 +9,7 @@ interface Circle {
   x: number;
   y: number;
   r: number;
+  deg: number;
 }
 import { Component, Vue } from 'vue-property-decorator';
 @Component
@@ -22,12 +23,15 @@ export default class CoilWave extends Vue {
   private canvas: HTMLCanvasElement | null = null;
   private ctx: CanvasRenderingContext2D | null = null;
   private style = {
+    bgFillStyle: 'white',
     lineWidth: 1,
     fillStyle: '#aaa',
     strokeStyle: '#888'
   };
   private option = {
-    scaleY: 0.2
+    scaleY: 0.2,
+    circleSpeed: 0.01, // 环运动速度
+    circleRange: 40 // 环运动范围
   };
   private circles: Circle[] = [];
   private mounted() {
@@ -39,6 +43,7 @@ export default class CoilWave extends Vue {
     if (!this.ctx) {
       return;
     }
+    const ctx = this.ctx;
     this.pannel.width = window.innerWidth;
     this.pannel.height = window.innerHeight;
     this.canvas.width = this.pannel.width;
@@ -46,7 +51,7 @@ export default class CoilWave extends Vue {
     this.pannel.centerX = this.pannel.width / 2;
     this.pannel.centerY = this.pannel.height / 2;
     this.generate();
-    this.draw();
+    this.draw(ctx);
   }
   private generate() {
     let i = 0;
@@ -54,16 +59,20 @@ export default class CoilWave extends Vue {
       this.circles.push({
         x: this.pannel.centerX,
         y: this.pannel.centerY,
-        r: i * 80
+        r: i * 80,
+        deg: i * 12
       });
     }
   }
-  private draw() {
-    this.drawCircle();
+  private draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = this.style.bgFillStyle;
+    ctx.fillRect(0, 0, this.pannel.width, this.pannel.height);
+    this.drawCircle(ctx);
+    requestAnimationFrame(() => {
+      this.draw(ctx);
+    });
   }
-  private drawCircle() {
-    const ctx = this.ctx;
-    if (!ctx) return;
+  private drawCircle(ctx: CanvasRenderingContext2D) {
     const circles = this.circles;
     const option = this.option;
     ctx.lineWidth = this.style.lineWidth;
@@ -71,18 +80,18 @@ export default class CoilWave extends Vue {
     ctx.strokeStyle = this.style.strokeStyle;
     ctx.save();
     ctx.scale(1, option.scaleY);
-
     circles.forEach((circle: Circle, index: number) => {
+      const dis = Math.sin(circle.deg) * option.circleRange;
+      circle.deg += option.circleSpeed;
       ctx.beginPath();
-      ctx.arc(circle.x, circle.y / option.scaleY, circle.r, 0, 2 * Math.PI);
-      ctx.closePath();
+      ctx.arc(circle.x, circle.y / option.scaleY - dis * 10, circle.r - dis, 0, 2 * Math.PI);
       if (index === 0) {
         ctx.fill();
       } else {
-        ctx.stroke(); // 先restore，后stroke，circle的线就是一样粗了(需要调整代码位置，不然只能画出最后一圈)
+        ctx.stroke();
       }
     });
-    ctx.restore();
+    ctx.restore(); // 先restore，后stroke，circle的线就是一样粗了
   }
 }
 </script>
